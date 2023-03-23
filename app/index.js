cc6490077ed4a3296abe8eca48a26f4b8440b1ff
Avishka Yohan <60248593+yohan98-dev@users.noreply.github.com>
2023-03-23 10:38:36 +0530

    Job deatails Page Done.

--- app/index.js
+++ app/index.js
@@ -36,7 +36,7 @@ const Home = () => {
         >
           <Welcome />
 
-          {/* <Popularjobs /> */}
+          <Popularjobs />
           <Nearbyjobs />
         </View>
       </ScrollView>
--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -21,7 +21,7 @@ import {
 import { COLORS, icons, SIZES } from "../../constants";
 import useFetch from "../../hook/useFetch";
 
-const tabs = ["About", "Qualification", "Resposibilities"];
+const tabs = ["About", "Qualifications", "Responsibilites"];
 
 const jobDetails = () => {
   const params = useSearchParams();
@@ -36,6 +36,33 @@ const jobDetails = () => {
   const [activeTab, setActiveTab] = useState(tabs[0]);
 
   const onRefresh = () => {};
+
+  const displayTabContent = () => {
+    switch (activeTab) {
+      case "Qualifications":
+        return (
+          <Specifics
+            title="Qualifications"
+            points={data[0].job_highlights?.Qualifications ?? ["N/A"]}
+          />
+        );
+      case "About":
+        return (
+          <JobAbout info={data[0].job_description ?? "No data provided"} />
+        );
+
+      case "Responsibilites":
+        return (
+          <Specifics
+            title="Responsibilites"
+            points={data[0].job_highlights?.Responsibilities ?? ["N/A"]}
+          />
+        );
+
+      default:
+        break;
+    }
+  };
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
@@ -86,9 +113,17 @@ const jobDetails = () => {
                 activeTab={activeTab}
                 setActiveTab={setActiveTab}
               />
+
+              {displayTabContent()}
             </View>
           )}
         </ScrollView>
+        <JobFooter
+          url={
+            data[0]?.job_google_link ??
+            "https://careers.google.com/jobs/results/"
+          }
+        />
       </>
     </SafeAreaView>
   );
